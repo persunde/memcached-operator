c521b99b97ce2aa3d1d18ce1719f1e3f7a72efaf
Per Sunde <sunde.per92@gmail.com>
2020-07-31 10:16:36 +0200

    fixed webserver timeout and added Docker push to makefile

--- webserver/webserver.js
+++ webserver/webserver.js
@@ -11,11 +11,11 @@ const server = http.createServer();
 server.on('request', async (req, res) => {
     const max = 10;
     const randInt = getRandomInt(max)
-    await sleep(100 * randInt);
+    await new Promise(r => setTimeout(r, 100 * randInt));
     totalrequests += 1
-    
-    response.writeHead(200);
-    response.end("Hello! My name is " + os.hostname() + ". I have served "+ totalrequests + " requests so far.\n");
+
+    res.writeHead(200);
+    res.end("Hello! My name is " + os.hostname() + ". I have served "+ totalrequests + " requests so far.\n");
 });
 
 server.listen(8080);
